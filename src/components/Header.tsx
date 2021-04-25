a7167010ea6eea0adc5449055a3b6606c894060d
ipiranhaa <tanatezp@gmail.com>
2021-04-25 11:54:12 +0700

    feat: add Github link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import { Input } from 'antd'
 
 import FilterIcon from './icons/Filter'
 import SortingIcon from './icons/Sorting'
+import GithubIcon from './icons/Github'
 
 interface Props {
   setSearchKeyword: (keyword: string[]) => void
@@ -17,10 +18,13 @@ const Header = ({ setSearchKeyword, setFilterModalShow, setSortingModalShow }: P
   }
 
   return (
-    <div className="flex content-between p-4 text-gray-800 text-xs">
+    <div className="flex gap-4 content-between p-4 text-gray-800 text-xs">
+      <a href="https://github.com/ipiranhaa/stock-z-lector-ui" rel="noreferrer" target="_blank">
+        <GithubIcon />
+      </a>
       <Input allowClear onChange={handleSearch} placeholder="SEARCH" />
       <button
-        className="ml-4 px-4 py-2 text-white text-sm bg-blue-500 active:bg-blue-600 rounded outline-none shadow hover:shadow-lg uppercase"
+        className="px-4 py-2 text-white text-sm bg-blue-500 active:bg-blue-600 rounded outline-none shadow hover:shadow-lg uppercase"
         onClick={() => setFilterModalShow(true)}
         style={{ transition: 'all .15s ease' }}
         type="button"
@@ -28,7 +32,7 @@ const Header = ({ setSearchKeyword, setFilterModalShow, setSortingModalShow }: P
         <FilterIcon />
       </button>
       <button
-        className="ml-4 px-4 py-2 text-white text-sm bg-blue-500 active:bg-blue-600 rounded outline-none shadow hover:shadow-lg uppercase"
+        className="px-4 py-2 text-white text-sm bg-blue-500 active:bg-blue-600 rounded outline-none shadow hover:shadow-lg uppercase"
         onClick={() => setSortingModalShow(true)}
         style={{ transition: 'all .15s ease' }}
         type="button"
